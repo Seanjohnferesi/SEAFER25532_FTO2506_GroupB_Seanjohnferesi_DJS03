b694858e90ff1130d6ad84591618dfbaf27bbd1d
Seanjohnferesi <seanjohnferesi27@gmail.com>
2025-11-08 02:01:51 +0200

    feat: fetch and render podcasts with loading and error states

--- src/components/PodcastCard.jsx
+++ src/components/PodcastCard.jsx
@@ -1,2 +1,41 @@
 import React from "react";
 import "../styles/PodcastCard.css";
+import  seasonIcon from "../assets/season.png"
+
+export default function PodcastCard({podcast}) {
+    const formatDate = (dateStr) => {
+        return new Date(dateStr).toLocaleDateString("en-US", {
+            day: "numeric",
+            month: "long",
+            year: "numeric"
+        });
+    }
+
+    return (
+        <>
+            <div className="card">
+                <div className="podcast-img-div">
+                    <img src={podcast.image} alt={podcast.title} className="podcast-img"/>
+                </div>
+
+                <h2 className="pod-title">{podcast.title}</h2>
+                        
+                <div className="season-wrapper">
+                    <img src={seasonIcon} alt="calender icon" className="season-icon"/>
+                    <span>{podcast.seasons} seasons</span>
+                </div>
+
+                <div className="genre-wrapper">
+                    <div className="genre-item">
+                        {}
+                    </div>
+                </div>
+
+                <p className="update-time">Updated {formatDate(podcast.updated)}</p>
+                {/* <p className="podcast-description">
+                    {podcast.description}
+                </p> */}
+            </div>
+        </>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
 import App from "./App";
-import "./styles/styles.css";
+import "./styles/App.css";
 
 const rootDom = document.getElementById("root")
 const root = ReactDOM.createRoot(rootDom);
